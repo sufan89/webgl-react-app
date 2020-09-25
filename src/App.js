9de5e03250ecbf22c01b56c54dba5a2ef1e574ec
sufan <sufan_89@hotmail.com>
2020-09-25 15:02:23 +0800

    update code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard></Dashboard>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
-
+function Home () {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  )
+}
+function About () {
+  return (
+    <div>
+      <h2>
+        About
+      </h2>
+    </div>
+  )
+}
+function Dashboard () {
+  return (
+    <div>
+      <h2>
+        Dashboard
+      </h2>
+    </div>
+  )
+}
 export default App;
