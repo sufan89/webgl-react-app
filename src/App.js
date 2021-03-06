f80bd3f20ce241be4e9ad55ee8c4bf82170d7bfd
sufan <sufan_89@hotmail.com>
2021-03-06 18:05:46 +0800

    添加ANT

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SimplePoint from "./demo/drawPoint/simplePoint";
+import { Button } from "antd";
 
 function App() {
   return (
@@ -44,6 +45,7 @@ function Home() {
   return (
     <div>
       <h2>Home</h2>
+      <Button type="primary">查询</Button>
     </div>
   );
 }
