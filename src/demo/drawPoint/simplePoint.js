8158e6a8b004651353d85b9f17db995b4f4569b7
sufan <sufan_89@hotmail.com>
2021-04-07 18:21:03 +0800

    update code

--- src/demo/drawPoint/simplePoint.js
+++ src/demo/drawPoint/simplePoint.js
@@ -1,14 +1,19 @@
 import React from "react";
 import curvegl from "../../curvegl";
-import fragementShader from "./shaders/point.frage";
-import vertextShader from "./shaders/point.vert";
+// import fragementShader from "./shaders/point.frage";
+// import vertextShader from "./shaders/point.vert";
+var glsl = require("glslify");
+// import { file } from "glslify";
 class SimplePoint extends React.Component {
   constructor(props) {
     super(props);
   }
   componentDidMount() {
     const gl = curvegl.setupWebGL("curvegl");
+    console.log(glsl);
     // console.log(fragementShader, vertextShader, "1212");
+    let vertextShader = glsl("file", "./shaders/point.vert.glsl");
+    let fragementShader = glsl("file", "./shaders/point.frage.glsl");
     curvegl.initShaders(gl, vertextShader, fragementShader);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
