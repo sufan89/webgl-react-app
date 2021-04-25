afeaaa27126b63661715cd5661a9fa3303a165b6
sufan <sufan_89@hotmail.com>
2021-04-25 18:13:08 +0800

    update code

--- src/demo/drawPoint/simplePoint.js
+++ src/demo/drawPoint/simplePoint.js
@@ -1,7 +1,9 @@
 import React from "react";
 import curvegl from "../../curvegl";
-// import fragementShader from "./shaders/point.frage";
-// import vertextShader from "./shaders/point.vert";
+// import fragementShader from "raw-loader!glslify-loader!./shaders/point.frage.glsl";
+// import vertextShader from "raw-loader!glslify-loader!./shaders/point.vert.glsl";
+var fragementShader = require("./shaders/point.frage.glsl");
+var vertextShader = require("./shaders/point.vert.glsl");
 var glsl = require("glslify");
 // import { file } from "glslify";
 class SimplePoint extends React.Component {
@@ -12,8 +14,16 @@ class SimplePoint extends React.Component {
     const gl = curvegl.setupWebGL("curvegl");
     console.log(glsl);
     // console.log(fragementShader, vertextShader, "1212");
-    let vertextShader = glsl("file", "./shaders/point.vert.glsl");
-    let fragementShader = glsl("file", "./shaders/point.frage.glsl");
+    // let vertextShader = glsl("file", "./shaders/point.vert.glsl");
+    // let vertextShader = require("./shaders/point.vert.glsl");
+    // let fragementShader = glsl("file", "./shaders/point.frage.glsl");
+    console.log(fragementShader, "fragementShader");
+    // let frag = require(fragementShader);
+    // console.log(frag, "frag");
+    let xr = new XMLHttpRequest();
+    xr.open("GET", fragementShader, true);
+    xr.send(null);
+
     curvegl.initShaders(gl, vertextShader, fragementShader);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
